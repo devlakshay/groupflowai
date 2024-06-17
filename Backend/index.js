c63f96403602ee91459aafd0e0a7bcbbb221b0f3
Yashwardhan Singh <138315503+YashwardhanSingh2005@users.noreply.github.com>
2024-06-18 01:52:05 +0530

    yash/startingwithbackend

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,15 @@
-import 'dotenv/config'
+import "dotenv/config";
 import express from "express";
+import validator from "validator";
 // const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+// Define your API endpoint for processing text data
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
