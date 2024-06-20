13c4ca75169f788632f8c21e0134ebce9a8d2115
Lakshya Goel <8059800706lakshya@gmail.com>
2024-06-20 14:44:48 +0530

    lakshya/minor changes

--- Backend/index.js
+++ Backend/index.js
@@ -5,7 +5,7 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 app.use(express.json());
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
