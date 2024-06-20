2e4c5644eb6519035e360c77687313db11da3b66
Yashwardhan Singh <138315503+YashwardhanSingh2005@users.noreply.github.com>
2024-06-21 01:51:03 +0530

    yash/added logo animation

--- Frontend/src/components/NavBar.tsx
+++ Frontend/src/components/NavBar.tsx
@@ -1,14 +1,14 @@
 import { HStack, Heading } from "@chakra-ui/layout";
 import ColorModeSwitch from "./ColorModeSwitch";
 import { Box, Image } from "@chakra-ui/react";
-
+import "./index.scss";
 const Navbar = () => {
   return (
     <HStack justifyContent={"space-between"}>
       <Box boxSize={20}>
         <Image src="https://static.wixstatic.com/media/977dce_eaf439279def4538a53230ab7726d14e~mv2.png" />
       </Box>
-      <Heading>Group Flow AI</Heading>
+      <Heading className="project">Group Flow AI</Heading>
       <ColorModeSwitch />
     </HStack>
   );
